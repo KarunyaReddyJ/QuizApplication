4acbb67a9f8974f3784f33c253dc9adda5f414d1
Karunya_Reddy <118816286+KarunyaReddyJ@users.noreply.github.com>
2024-09-23 22:58:15 +0530

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
-
-import Result  from './components/Result'
-import QuestionBox from './components/QuestionBox'
-import SideBar from './components/SideBar'
-import useData from './CustomHook/useData'
+import Result from './components/Result';
+import QuestionBox from './components/QuestionBox';
+import SideBar from './components/SideBar';
+import useData from './CustomHook/useData';
 
 function App() {
-  const {End,score}=useData()
-  return (
-    
-      <div className='flex flex-row relative'>
-    {
-      End? <Result/>
-      :
-      <>
-      <QuestionBox/>
-      <SideBar/></>
+  const { End } = useData();
 
-    }
-     </div>
-    
-  )
+  return (
+    <div className="flex flex-row min-h-screen relative">
+      {
+        End ? (
+         
+          <div className="flex items-center justify-center w-full">
+            <Result />
+          </div>
+        ) : (
+          <div className="flex flex-row w-full">
+
+            <div className="w-3/4 p-4">
+              <QuestionBox />
+            </div>
+
+           
+            <div className="w-1/4 bg-gray-100 p-4">
+              <SideBar />
+            </div>
+          </div>
+        )
+      }
+    </div>
+  );
 }
 
-export default App
+export default App;
