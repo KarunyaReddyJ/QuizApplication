dc356ad23b6395e0cce7446a3852c780bbb3637a
Karunya_Reddy <118816286+KarunyaReddyJ@users.noreply.github.com>
2024-09-24 08:48:14 +0530

    Update Result.jsx

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -7,7 +7,7 @@ function Result() {
   return (
     <div className="text-black w-6/12 flex flex-col items-center justify-center h-screen bg-gray-50">
     
-      <div className="bg-white shadow-lg rounded-lg p-8 text-center animate-fadeIn">
+      <div className="bg-white shadow-lg rounded-lg p-8 text-center">
         <h2 className="text-3xl font-semibold text-indigo-600 mb-4">Quiz Completed!</h2>
         <p className="text-xl text-gray-700">Your Score:</p>
         <p className="text-4xl font-bold text-green-500">{score}</p>
